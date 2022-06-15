b8a1a34302171cffdd090c48ae09b141b65f32c5
Matthew Buddensick <matthewbuddensick@gmail.com>
2022-06-14 22:39:15 -0400

    Added more charts/tables

--- app.R
+++ app.R
@@ -47,16 +47,15 @@ cleaned_databetic_data <- diabetic_data %>%
          medical_specialty = ifelse(is.na(medical_specialty), "Unknown", medical_specialty),
          readmitted = case_when(
            readmitted == "NO" ~ "No Readmission",
-           readmitted == ">30" ~ "Readmission in greater than 30 days",
-           readmitted == "<30" ~ "Readmission in less than 30 days")) 
+           readmitted == ">30" ~ "Readmission >30 days",
+           readmitted == "<30" ~ "Readmission <30 days")) 
 
 # Define UI for application
 ui <- fluidPage(
   # Application title
   titlePanel(h3("Diabetes in 130 US hospitals (1999-2008)")),
-  titlePanel(h5("Information on Data: https://www.kaggle.com/datasets/brandao/diabetes")),
   tabsetPanel(
-    tabPanel("Demographics",
+    tabPanel("Patient Demographics",
              # Sidebar with a slider input for number of bins 
              sidebarLayout(position = "left",
                            sidebarPanel(
@@ -75,15 +74,18 @@ ui <- fluidPage(
                                          multiple = T, options = list(`actions-box` = TRUE),
                                          selected = unique(cleaned_databetic_data$discharge_disposition))),
                            mainPanel(
-                             plotOutput("demoChart")
+                             tableOutput("exampleData"), plotOutput("demoChart")
                            )
              )
     ),
-    tabPanel("Hospital Data",
+    tabPanel("Patient Data",
              sidebarLayout(position = "left",
                            sidebarPanel(
                              sliderInput("selectTimeInHospital", label = "Select Days in Hospital",
                                          min = 1, max = 14, value = c(1, 14)),
+                             selectInput("selectChartY", label = "Select Chart Axis",
+                                         choices = c("Race" = "race", "Gender" = "gender", 
+                                                     "Age" = "age", "Medical Specialty")),
                              pickerInput("selectReadmission", label = "Select Readmission Time", 
                                          choices = unique(cleaned_databetic_data$readmitted), 
                                          multiple = T, options = list(`actions-box` = TRUE),
@@ -93,7 +95,7 @@ ui <- fluidPage(
                                          multiple = T, options = list(`actions-box` = TRUE),
                                          selected = unique(cleaned_databetic_data$diabetesMed))),
                            mainPanel(
-                             tableOutput("stayInHospital")
+                             plotOutput("readmission"), plotOutput("diagnoses")
                            )
              )
     )
@@ -109,9 +111,20 @@ server <- function(input, output) {
     filter(admission_type %in% input$selectAdmissionType & admission_source %in% input$selectAdmissionSource &
              discharge_disposition %in% input$selectDischargeDisposition)})
   
+  output$exampleData <- renderTable({
+    demographic_diabetic_data() %>% 
+      distinct(patient_nbr) %>% 
+      mutate(`Total Number of Patients` = nrow(demographic_diabetic_data() %>% 
+                                                 distinct(patient_nbr))) %>% 
+      slice_max(1) %>% 
+      select(`Total Number of Patients`)
+  })
+  
   output$demoChart <- renderPlot({
     if (input$selectDemoChart == "Race") {
       ggplot(demographic_diabetic_data() %>% 
+               select(race, patient_nbr) %>% 
+               distinct() %>% 
                group_by(race) %>% 
                count() %>% 
                arrange(-n) %>% 
@@ -121,26 +134,31 @@ server <- function(input, output) {
              aes(y = Frequency, x = fct_reorder(as.factor(Race), -Frequency)), color = "#1b94e4") +
         geom_col(fill = "#1b94e4") +
         theme_classic() +
-        labs(x = "Race", y = "Number of Patients") +
-        geom_text(aes(label = Frequency), vjust = -0.5)
+        labs(x = "", y = "", titles = "Patients By Race") +
+        geom_text(aes(label = Frequency), vjust = -0.2) +
+        theme(axis.text.x = element_text(size = 8), axis.text.y = element_text(size = 8))
     }
     else if (input$selectDemoChart == "Gender") {
       ggplot(demographic_diabetic_data() %>% 
+               select(gender, patient_nbr) %>% 
+               distinct() %>% 
                group_by(gender) %>% 
-               count() %>% 
+               count() %>%  
                arrange(-n) %>% 
                rename(Gender = gender,
                       Frequency = n), aes(y = Frequency, x = fct_reorder(as.factor(Gender), -Frequency))) +
         geom_col(fill = "#1b94e4") +
         theme_classic() +
-        labs(x = "Gender", y = "Number of Patients") +
-        geom_text(aes(label = Frequency), vjust = -0.5)
+        labs(x = "", y = "", title = "Patients by Gender") +
+        geom_text(aes(label = Frequency), vjust = -0.2) +
+        theme(axis.text.x = element_text(size = 8), axis.text.y = element_text(size = 8))
     }
     else {
       ggplot(demographic_diabetic_data() %>% 
+               select(age, patient_nbr) %>% 
+               distinct() %>% 
                group_by(age) %>% 
                count() %>% 
-               arrange(-n) %>% 
                rename(Age = age,
                       Frequency = n), aes(x = as.factor(Age), 
                                           order = c("[0-10)", "[10-20)", "[20-30)", "[30-40)",
@@ -149,17 +167,19 @@ server <- function(input, output) {
         geom_col() +
         theme_classic() +
         theme(legend.position = "none") +
-        labs(x = "Age", y = "Number of Patients") +
-        geom_text(aes(label = Frequency), vjust = -0.5)
+        labs(x = "", y = "", title = "Patients by Age") +
+        geom_text(aes(label = Frequency), vjust = -0.2) +
+        theme(axis.text.x = element_text(size = 8), axis.text.y = element_text(size = 8))
     }
   }, res = 96)
   
   
   # Second Tab Panel
   hosipital_diabetic_data <- reactive({cleaned_databetic_data %>% 
-      select(race, gender, age, time_in_hospital, num_lab_procedures, num_procedures,
+      select(race, gender, age, time_in_hospital, num_lab_procedures, num_procedures, patient_nbr,
              number_emergency, readmitted, medical_specialty, number_diagnoses, admission_type,
              discharge_disposition, admission_source, readmitted, diabetesMed) %>% 
+      mutate(medical_specialty = fct_lump(medical_specialty, 10)) %>% 
       filter(time_in_hospital >= input$selectTimeInHospital[1] &
                time_in_hospital <= input$selectTimeInHospital[2] &
                readmitted %in% input$selectReadmission &
@@ -168,12 +188,78 @@ server <- function(input, output) {
   })
            
            
-  output$stayInHospital <- renderTable({
+  output$readmission <- renderPlot({
+    if (input$selectChartY == "Medical Specialty") {
+      hosipital_diabetic_data() %>% 
+        group_by(medical_specialty) %>% 
+        count(readmitted) %>% 
+        ggplot(aes(y = medical_specialty, x = n, fill = readmitted)) +
+        geom_col(position = "fill") +
+        scale_fill_brewer(palette = "Blues") +
+        theme_classic() +
+        labs(x = "", y = "", fill = "Readmission", title = "Patient Readmissions") +
+        scale_x_continuous(labels = scales::percent_format()) +
+        theme(axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10))
+    }
+    else {
+      hosipital_diabetic_data() %>% 
+        group_by(!!as.symbol(input$selectChartY)) %>% 
+        count(readmitted) %>%  
+        ggplot(aes_string(y = input$selectChartY, x = "n", fill = "readmitted")) +
+        geom_col(position = "fill") +
+        scale_fill_brewer(palette = "Blues") +
+        theme_classic() +
+        labs(x = "", y = "", fill = "Readmission", title = "Patient Readmissions") +
+        scale_x_continuous(labels = scales::percent_format()) +
+        theme(axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10))
+    }
+  }, res = 96)
+  
+  output$diagnoses <- renderPlot({
+    if (input$selectChartY == "Medical Specialty") {
     hosipital_diabetic_data() %>% 
-      select(race, gender, age, time_in_hospital, num_lab_procedures, num_procedures,
-             number_emergency, readmitted, medical_specialty, number_diagnoses) %>% 
-      mutate(medical_specialty = ifelse(is.na(medical_specialty), "Unknown", medical_specialty))
-  })
+      group_by(patient_nbr, medical_specialty) %>% 
+      summarise(total_diagnoses = sum(number_diagnoses)) %>% 
+      group_by(medical_specialty) %>% 
+      summarise(avg_diagnoses = mean(total_diagnoses)) %>% 
+      ggplot(aes(y = fct_reorder(medical_specialty,  avg_diagnoses), x = avg_diagnoses)) +
+      geom_col(fill = "#1b94e4") +
+      geom_text(aes(label = round(avg_diagnoses, 2))) +
+      labs(x = "", y = "", title = "Average Diagnoses") +
+      theme_classic() +
+      theme(legend.position = "none") +
+      scale_color_distiller()
+    }
+    else if (input$selectChartY == "Age") {
+      hosipital_diabetic_data() %>% 
+        group_by(patient_nbr, age) %>% 
+        summarise(total_diagnoses = sum(number_diagnoses)) %>% 
+        group_by(age) %>% 
+        summarise(avg_diagnoses = mean(total_diagnoses)) %>% 
+        ggplot(aes(x = as.factor(age), 
+                   order = c("[0-10)", "[10-20)", "[20-30)", "[30-40)",
+                             "[40-50)", "[50-60)","[60-70)",
+                             "[70-80)", "[80-90)", "[90-100)"), y = avg_diagnoses)) +
+        geom_col(fill = "#1b94e4") +
+        geom_text(aes(label = round(avg_diagnoses, 2), vjust = -0.2)) +
+        labs(x = "", y = "", title = "Average Diagnoses") +
+        theme_classic() +
+        theme(legend.position = "none")
+    }
+    else {
+      hosipital_diabetic_data() %>% 
+        group_by(patient_nbr, !!as.symbol(input$selectChartY)) %>% 
+        summarise(total_diagnoses = sum(number_diagnoses)) %>% 
+        group_by(!!as.symbol(input$selectChartY)) %>% 
+        summarise(avg_diagnoses = mean(total_diagnoses)) %>% 
+        ggplot(aes(x = fct_reorder(!!as.symbol(input$selectChartY),  -avg_diagnoses), y = avg_diagnoses)) +
+        geom_col(fill = "#1b94e4") +
+        geom_text(aes(label = round(avg_diagnoses, 2), vjust = -0.2)) +
+        labs(x = "", y = "", title = "Average Diagnoses") +
+        theme_classic() +
+        theme(legend.position = "none")
+    }
+  }, res = 96)
 }
 
 # Run the application 
